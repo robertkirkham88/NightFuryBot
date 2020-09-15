a1f79e39a44c90faa5d49015b63fe48621a77edb
Robert Kirkham <rsk1588.10+github@gmail.com>
2020-09-15 12:05:33 +0100

    Add NFB.Service.Vatsim

---
+++ src/NFB.Service.Vatsim/Persistence/VatsimDbContext.cs
@@ -0,0 +1,25 @@
+﻿namespace NFB.Service.Vatsim.Persistence
+{
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// The flight database context.
+    /// </summary>
+    public class VatsimDbContext : DbContext
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VatsimDbContext"/> class.
+        /// </summary>
+        /// <param name="options">
+        /// The options.
+        /// </param>
+        public VatsimDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        #endregion Public Constructors
+    }
+}
---
+++ src/NFB.Service.Vatsim/Persistence/VatsimDbContextDesignTime.cs
@@ -0,0 +1,30 @@
+﻿namespace NFB.Service.Vatsim.Persistence
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+
+    /// <summary>
+    /// The flight db context design time.
+    /// </summary>
+    public class VatsimDbContextDesignTime : IDesignTimeDbContextFactory<VatsimDbContext>
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Create a new design time context for migrations.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        /// <returns>
+        /// The <see cref="VatsimDbContext"/>.
+        /// </returns>
+        public VatsimDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<VatsimDbContext>().UseNpgsql("Host=localhost;Database=VatsimService-Migrations-150920;Username=superuser;Password=z1Tf3U9io5ufol").Options;
+            return new VatsimDbContext(options);
+        }
+
+        #endregion Public Methods
+    }
+}
