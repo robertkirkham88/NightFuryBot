a3666d7fe35806897dab180b2d0e0897f8efb417
Robert Kirkham <rsk1588.10+github@gmail.com>
2020-09-21 15:42:38 +0100

    Mock objects.

---
+++ tests/NFB.UI.DiscordBot.Tests/TestFactory/CategoryChannelTestObject.cs
@@ -0,0 +1,93 @@
+﻿namespace NFB.UI.DiscordBot.Tests.TestFactory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Discord;
+
+    /// <summary>
+    /// The category channel test object.
+    /// </summary>
+    public class CategoryChannelTestObject : ICategoryChannel
+    {
+        #region Public Properties
+
+        public IList<ITextChannel> Channels { get; set; } = new List<ITextChannel>();
+        public DateTimeOffset CreatedAt { get; }
+        public IGuild Guild { get; }
+        public ulong GuildId { get; }
+        public ulong Id { get; }
+        public string Name { get; set; }
+
+        public IReadOnlyCollection<Overwrite> PermissionOverwrites { get; }
+
+        public int Position { get; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public Task AddPermissionOverwriteAsync(IRole role, OverwritePermissions permissions, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddPermissionOverwriteAsync(IUser user, OverwritePermissions permissions, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OverwritePermissions? GetPermissionOverwrite(IRole role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OverwritePermissions? GetPermissionOverwrite(IUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IGuildUser> IGuildChannel.GetUserAsync(ulong id, CacheMode mode, RequestOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IUser> IChannel.GetUserAsync(ulong id, CacheMode mode, RequestOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<IReadOnlyCollection<IGuildUser>> GetUsersAsync(CacheMode mode = CacheMode.AllowDownload, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncEnumerable<IReadOnlyCollection<IUser>> IChannel.GetUsersAsync(CacheMode mode, RequestOptions options)
+        {
+            return this.GetUsersAsync(mode, options);
+        }
+
+        public Task ModifyAsync(Action<GuildChannelProperties> func, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePermissionOverwriteAsync(IRole role, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePermissionOverwriteAsync(IUser user, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ tests/NFB.UI.DiscordBot.Tests/TestFactory/TextChannelTestObject.cs
@@ -0,0 +1,250 @@
+﻿namespace NFB.UI.DiscordBot.Tests.TestFactory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using Discord;
+
+    /// <summary>
+    /// The text channel test object.
+    /// </summary>
+    public class TextChannelTestObject : ITextChannel
+    {
+        #region Public Properties
+
+        public ulong? CategoryId { get; }
+        public DateTimeOffset CreatedAt { get; }
+        public IGuild Guild { get; }
+        public ulong GuildId { get; }
+        public virtual ulong Id { get; set; }
+        public bool IsNsfw { get; }
+
+        public string Mention { get; }
+
+        public string Name { get; set; }
+
+        public IReadOnlyCollection<Overwrite> PermissionOverwrites { get; }
+
+        public int Position { get; }
+
+        public int SlowModeInterval { get; }
+
+        public string Topic { get; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public Task AddPermissionOverwriteAsync(IRole role, OverwritePermissions permissions, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddPermissionOverwriteAsync(IUser user, OverwritePermissions permissions, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IInviteMetadata> CreateInviteAsync(
+            int? maxAge,
+            int? maxUses = null,
+            bool isTemporary = false,
+            bool isUnique = false,
+            RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IWebhook> CreateWebhookAsync(string name, Stream avatar = null, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMessageAsync(ulong messageId, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMessageAsync(IMessage message, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMessagesAsync(IEnumerable<IMessage> messages, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMessagesAsync(IEnumerable<ulong> messageIds, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable EnterTypingState(RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICategoryChannel> GetCategoryAsync(CacheMode mode = CacheMode.AllowDownload, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyCollection<IInviteMetadata>> GetInvitesAsync(RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<IMessage> GetMessageAsync(ulong id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IMessage> GetMessageAsync(ulong id, CacheMode mode = CacheMode.AllowDownload, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<IReadOnlyCollection<IMessage>> GetMessagesAsync(int limit = 100, CacheMode mode = CacheMode.AllowDownload, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<IReadOnlyCollection<IMessage>> GetMessagesAsync(
+            ulong fromMessageId,
+            Direction dir,
+            int limit = 100,
+            CacheMode mode = CacheMode.AllowDownload,
+            RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<IReadOnlyCollection<IMessage>> GetMessagesAsync(
+            IMessage fromMessage,
+            Direction dir,
+            int limit = 100,
+            CacheMode mode = CacheMode.AllowDownload,
+            RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OverwritePermissions? GetPermissionOverwrite(IRole role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OverwritePermissions? GetPermissionOverwrite(IUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyCollection<IMessage>> GetPinnedMessagesAsync(RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IGuildUser> IGuildChannel.GetUserAsync(ulong id, CacheMode mode, RequestOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IUser> IChannel.GetUserAsync(ulong id, CacheMode mode, RequestOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<IReadOnlyCollection<IGuildUser>> GetUsersAsync(CacheMode mode = CacheMode.AllowDownload, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncEnumerable<IReadOnlyCollection<IUser>> IChannel.GetUsersAsync(CacheMode mode, RequestOptions options)
+        {
+            return this.GetUsersAsync(mode, options);
+        }
+
+        public Task<IWebhook> GetWebhookAsync(ulong id, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyCollection<IWebhook>> GetWebhooksAsync(RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ModifyAsync(Action<GuildChannelProperties> func, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ModifyAsync(Action<TextChannelProperties> func, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePermissionOverwriteAsync(IRole role, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePermissionOverwriteAsync(IUser user, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IUserMessage> SendFileAsync(
+            string filePath,
+            string text = null,
+            bool isTTS = false,
+            Embed embed = null,
+            RequestOptions options = null,
+            bool isSpoiler = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IUserMessage> SendFileAsync(
+            Stream stream,
+            string filename,
+            string text = null,
+            bool isTTS = false,
+            Embed embed = null,
+            RequestOptions options = null,
+            bool isSpoiler = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IUserMessage> SendMessageAsync(string text = null, bool isTTS = false, Embed embed = null, RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<IUserMessage> SendMessageAsync(Embed embed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SyncPermissionsAsync(RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TriggerTypingAsync(RequestOptions options = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Public Methods
+    }
+}
